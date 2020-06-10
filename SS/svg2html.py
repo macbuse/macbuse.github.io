1d4a3b9c5f9b3332c84c52ef792e3e956ee85e5f
macbuse <greg.mcshane@gmail.com>
2020-06-10 10:05:16 +0200

    web

---
+++ SS/svg2html.py
@@ -0,0 +1,105 @@
+#! /home/gregmcshane/anaconda3/bin/python3.6 
+'''
+embed an svg file in html
+with css animation
+'''
+
+import re, sys,  argparse
+from svgpathtools import svg2paths
+
+HTML = '''<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8" />
+  <style>
+  body {{
+  background-color: #ffffff;
+    }}
+  {}
+  </style>
+    
+</head>
+<body>
+{}
+</body>
+  '''
+
+ANIM_PARAMS = ''' 
+#{} {{ stroke-dasharray:  {};
+      stroke-dashoffset: {};
+      opacity : 0;
+      animation: offset {:.2f}s linear forwards;
+      animation-delay: {:.2f}s;
+      animation-iteration-count: 1;
+    }} '''
+
+ANIM_LOOP = '''
+@keyframes offset {
+    to {
+    stroke-dashoffset: 0;  opacity: 1;
+    }
+'''
+
+PP_PATH = re.compile('<path(.*?)\sd=', re.DOTALL)
+PP_METADATA = re.compile('<metadata>.*?</metadata>',re.DOTALL)
+PP_UNITS = re.compile('(\d+)pt')
+
+def units_cb(mm):
+    return '%spx'%mm.group(1)
+
+def mk_anim(src_fn, 
+            speed=1.,
+            top_first=False):
+    
+    def path_cb(mm):
+        return '<path id="%s" %s d='%(path_ids.pop(0), mm.group(1))
+
+    with open(src_fn, 'r') as FP:
+        data = FP.read()
+    
+    #ditch the metadata - there's a thumbnail :(
+    data = re.sub(PP_METADATA, '', data)
+
+    paths, attributes = svg2paths(src_fn)
+    path_ids = ['p%d'%k for k, x in enumerate(paths)]
+    path_ids_cp = path_ids[:] #make a copy for debugging
+
+    data = re.sub(PP_UNITS, units_cb, data)
+    svg = re.sub(PP_PATH, path_cb, data)
+
+    paths, attributes = svg2paths(src_fn)
+
+    lengths = [int(x.length()) for x in paths]
+
+    rolling = [0]
+    for cv_len in lengths:
+        rolling.append(rolling[-1] + cv_len)
+    if top_first:
+        rolling = [rolling[-1] - x for x in rolling]
+
+    animations = [ ANIM_PARAMS.format('p%d'%k,
+                                     length,
+                                     length,
+                                     length/500/speed, #duration in secs
+                                     delay/500/speed  #delay in secs
+                                     ) 
+         for k,length,delay in zip(range(len(lengths)), lengths, rolling)  ]
+
+    animations.append(ANIM_LOOP)
+    style = ' '.join(animations)
+
+    with open('%s.html'%src_fn.split('.')[0], 'w') as FP:
+        FP.write(HTML.format(style, svg))
+
+if __name__ == '__main__':
+    PARSER = argparse.ArgumentParser()
+    PARSER.add_argument("FN")
+    PARSER.add_argument("-s", dest='speed', default=1.0)
+    PARSER.add_argument("-r", dest='r', action='store_true', default=False)
+    ARGS = PARSER.parse_args()
+
+    if len(sys.argv) < 2:
+        print('usage: %s file.svg'%sys.argv[0])
+        sys.exit(1)
+
+    mk_anim(ARGS.FN, speed=float(ARGS.speed), top_first=ARGS.r)
---
+++ SS/tmp.md
@@ -0,0 +1,35 @@
+%SVG-land
+%greg mc
+%June 2020
+
+# 
+
+```
+A long long time ago  
+in a galaxy far away.....  
+
+I think I am mad 
+to have done this.
+```
+
+#
+
+```
+If truth be told....
+
+I know I am mad 
+to have done it.
+```
+#
+##  Well slighty crazy
+
+<div class="wrap"><iframe data-src="sw.html" > </iframe></div>
+
+
+<audio src="sw.mp3" data-autoplay> </audio>
+
+#
+<div class="wrap"><iframe data-src="c3po.html" > </iframe></div>
+
+
+<audio  data-autoplay ><source src="i_know.mp3" ></audio> 
