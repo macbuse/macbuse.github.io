21e1da927d8a2c0d1819c8f4f8003d3bc24bf2b2
macbuse <greg.mcshane@gmail.com>
2022-06-30 20:16:29 +0200

    web

--- PROG/epidemic_sim.py
+++ PROG/epidemic_sim.py
@@ -14,7 +14,6 @@ ymax = 900 #Height of screen in pixels
 screen = pygame.display.set_mode((xmax, ymax), 0, 24) #New 24-bit screen
 pygame.display.set_caption('Simulation of an epidemic')
 
-
 class Grid():
     
     def __init__(self, sz = 40):
@@ -72,7 +71,7 @@ def draw_block(x, y, color):
     center_point = ((x + (BLOCK_SIZE / 2)), (y + (BLOCK_SIZE / 2)))
     pygame.draw.circle(screen, color, center_point, BLOCK_SIZE / 2,0)
 
-def update_screen(grid):
+def update_screen(world):
 
     ALIVE_COLOR = pygame.Color(0,100,100)
     INFECTED_COLOR = pygame.Color(250,10,10)
@@ -80,18 +79,17 @@ def update_screen(grid):
 
     screen.fill((0,0,0))
 
-    for x,y in grid.walls:
+    for x,y in world.walls:
         draw_block(x, y, WALL_COLOR)
-    for x,y in np.array(np.where(grid == 2)).T:
+    for x,y in np.array(np.where(world.G == UNINFECTED)).T:
         draw_block(x, y, ALIVE_COLOR)
-    for x,y in np.array(np.where(grid == 7)).T:
+    for x,y in np.array(np.where(world.G == INFECTED)).T:
         draw_block(x, y, INFECTED_COLOR)
-
 
     pygame.display.flip()
 
-
 def main():
+
     def clip(x):
         # don't look outside the box
         # sz is global
@@ -100,7 +98,6 @@ def main():
         return x
 
     # initialise
-
     sz = 100
     world  = Grid(sz=sz)
     world.W[47] = 1
@@ -109,7 +106,6 @@ def main():
     # mk_moves should get called just once after the walls are set
     world.mk_moves()
     world.walls = np.array(np.where(world.W[1:-1,1:-1] == 1)).T 
-    # print(world.walls)
 
     sz -= 2
     #initial configuration evenly spaced
@@ -120,50 +116,32 @@ def main():
             
     pts = np.array(pp)
     #choose who is infected
+    global INFECTED, UNINFECTED
     UNINFECTED = 2
     INFECTED = 7
     agent_state = UNINFECTED*np.ones(len(pp))
     agent_state[44] = agent_state[-36] =  agent_state[-1] = INFECTED
 
     # do the event loop
-          
     stats_now = []
     for k in range(1000):
+        #update world state
         world.set_blank() 
-        #update
         for pt, state in zip(pts, agent_state):
             pos = tuple(pt)
             world.G[pos] = state
             pt += world.get_move(pos)
         
-
-        ALIVE_COLOR = pygame.Color(0,100,100)
-        INFECTED_COLOR = pygame.Color(250,10,10)
-        WALL_COLOR = pygame.Color(250,250,10)
-
-        screen.fill((0,0,0))
-
-        for x,y in world.walls:
-            draw_block(x, y, WALL_COLOR)
-        for x,y in np.array(np.where(world.G == 2)).T:
-            draw_block(x, y, ALIVE_COLOR)
-        for x,y in np.array(np.where(world.G == 7)).T:
-            draw_block(x, y, INFECTED_COLOR)
-
-
-        pygame.display.flip()  
-        # update_screen(world)
-
-        #count infected, should be dumped to a file at the end
-        stats_now.append(len(agent_state[agent_state==INFECTED]))
-        # print(stats_now)
-
-        # I have to recalculate this every time  
         for _ in np.where(agent_state == UNINFECTED)[0]:
             x, y = pts[_]
             neighbors = world.G[clip(x-1):clip(x+2), clip(y-1):clip(y+2) ]
             if np.max(neighbors) == INFECTED:
                 agent_state[_] = INFECTED
+
+        #count infected, should be dumped to a file at the end
+        stats_now.append(len(agent_state[agent_state==INFECTED]))
+
+        update_screen(world)
         sleep(0.1)
 
     #dump the data for analysis
